4cdf04d7ba958e90f8a8e9b8782af055a38e4020
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2022-09-02 14:14:46 -0300

    feat: ugrade k8 and remove public available rds

--- eks-infra/rds.tf
+++ eks-infra/rds.tf
@@ -51,6 +51,5 @@ resource "aws_db_instance" "education" {
   db_subnet_group_name   = aws_db_subnet_group.education.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.education.name
-  publicly_accessible    = true
   skip_final_snapshot    = true
 }
